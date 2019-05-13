523acab4da240e3795082d0ff0f6b8a9c8a8a922
birdmanwings <747458025@qq.com>
2019-05-13 14:06:03 +0800

    使用docker-compose

--- bdwms_blog/wsgi.py
+++ bdwms_blog/wsgi.py
@@ -1,10 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')  # .env文件所在的地方
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')  # .env文件所在的地方
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-from bdwms_blog import create_app
+from bdwms_blog.__init__ import create_app
 
 app = create_app('production')  # 使用production生产环境，gunicorn wsgi容器将发现这个create_app函数
--- start_server.sh
+++ start_server.sh
@@ -3,4 +3,5 @@
 flask initdb
 
 # 启动服务，使用exec以防止另起进程
+cd bdwms_blog
 exec gunicorn -w 4 -b 0.0.0.0:8000 wsgi:app
