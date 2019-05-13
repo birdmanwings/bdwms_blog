576314ed2da7973a8b78d78e196c714572534399
birdmanwings <747458025@qq.com>
2019-05-13 14:12:09 +0800

    使用docker-compose

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')  # .env文件所在的地方
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')  # .env文件所在的地方
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
